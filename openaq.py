7b5a350fb873a4e89d815b991bb30c5f045a3a43
habermoose <michaelghabermas@gmail.com>
2025-10-06 16:50:45 -0400

    Start openai.py file

--- openaq.py
+++ openaq.py
@@ -1,1 +1,11 @@
-# openaq.py OpenAQ API wrapper (download from course)
+"""Simple openaq to only depend on json, math, and requests (no dfs/plots)."""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+import math
+
+
+class ApiError(Exception):
+    pass
