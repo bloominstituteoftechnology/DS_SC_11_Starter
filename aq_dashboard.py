9c2fdad6f0be8fd4a60e76048d1c190b1f1af7d6
habermoose <michaelghabermas@gmail.com>
2025-10-07 12:39:39 -0400

    initialize flask in dashboard

--- aq_dashboard.py
+++ aq_dashboard.py
@@ -5,3 +5,13 @@ Complete the TODO items to build a fully functional dashboard.
 
 Setup Instructions:
 1. Install dependencies: pip install flask flask-sqlalchemy requests py-openaq pytest
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from openaq import OpenAQ
+
+# Initialize Flask app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+DB = SQLAlchemy(app)
