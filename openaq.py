89f105d240ee44e100d614ff0da0b6283069e886
habermoose <michaelghabermas@gmail.com>
2025-10-07 12:38:46 -0400

    OPen aq API class

--- openaq.py
+++ openaq.py
@@ -9,3 +9,17 @@ import math
 
 class ApiError(Exception):
     pass
+
+
+class API:
+    """Generic API wrapper object."""
+
+    def __init__(self, **kwargs):
+        self._key = kwargs.pop('key', '')
+        self._pswd = kwargs.pop('pswd', '')
+        self._version = kwargs.pop('version', None)
+        self._baseurl = kwargs.pop('baseurl', None)
+        self._headers = {'content-type': 'application/json'}
+        # Add API key to headers if provided
+        if self._key:
+            self._headers['X-API-Key'] = self._key
