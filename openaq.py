0e9ea4dd85a97c7a7c005905d15fe07bbeeeae50
habermoose <michaelghabermas@gmail.com>
2025-10-08 17:25:38 -0400

    Add method for making API calls in openaq.py, supporting GET requests and handling response metadata.

--- openaq.py
+++ openaq.py
@@ -45,3 +45,38 @@ class API:
             endpoint = '?'.join([endpoint, '&'.join(extra)])
 
         return endpoint
+
+    def _send(self, endpoint, method='GET', **kwargs):
+        """Make an API call of any method
+
+        :param endpoint: API endpoint
+        :param method: API call type. Options are PUT, POST, GET, DELETE
+
+        :type endpoint: string
+        :type method: string
+
+        :returns: (status_code, json_response)
+
+        :raises ApiError: raises an exception
+        """
+        # For OpenAQ API v3, use API key in headers instead of basic auth
+        auth = (self._key, self._pswd) if self._pswd else None
+        url = self._make_url(endpoint, **kwargs)
+
+        if method == 'GET':
+            resp = requests.get(url, auth=auth, headers=self._headers)
+        else:
+            raise ApiError("Invalid Method")
+
+        if resp.status_code != 200:
+            raise ApiError("A bad request was made: {}".format(resp.status_code))
+
+        res = resp.json()
+
+        # Add a 'pages' attribute to the meta data
+        try:
+            res['meta']['pages'] = math.ceil(res['meta']['found'] / res['meta']['limit'])
+        except:
+            pass
+
+        return resp.status_code, res
